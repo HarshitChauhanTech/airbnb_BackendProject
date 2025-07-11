0b2481f92a23df705347e44e90baa1ea59386101
HarshitChauhanTech <chauhan.harshitsingh2004@gmail.com>
2025-07-11 22:59:06 +0530

    MVC Learning with project

---
+++ routes/hostRouter.js
@@ -0,0 +1,16 @@
+// Core Module
+
+// External Module
+const express = require('express');
+const hostRouter = express.Router();
+
+// Local Module
+const rootDir = require("../utils/pathUtil");
+const { getAddHome, postAddHome } = require('../controllers/homes');
+
+hostRouter.get("/add-home", getAddHome)
+hostRouter.post("/add-home",postAddHome)
+
+
+exports.hostRouter = hostRouter;
+
